28fb0117e9dc0b427995767c146644b65efab792
Bintang Muhammad <47523028+edwardjacob2315@users.noreply.github.com>
2022-03-04 11:14:28 +0700

    Upload Files

---
+++ milestone.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from scipy import stats
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import datetime
+import streamlit.st
+
+st.title('Milestone Bintang Muhammad')
+
+
+
+
